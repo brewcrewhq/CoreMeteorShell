421323d6c278623c42db8fa42a0cad5055073432
Brew <larrybrewer@cox.net>
2015-10-04 21:59:25 -0700

    Moved accounts.ui config to client > config

---
+++ client/config/accounts.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Brew on 10/4/2015.
+ */
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,3 @@
 /**
  * Created by Brew on 10/2/2015.
  */
-Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
-});
