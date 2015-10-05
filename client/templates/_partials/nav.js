159be93dba0ece46be59871733c72e10e8053a3a
Brew <larrybrewer@cox.net>
2015-10-04 22:07:32 -0700

    Fixed login to not need spacer.gif.

--- client/templates/_partials/nav.js
+++ client/templates/_partials/nav.js
@@ -7,19 +7,3 @@ Template.nav.helpers({
         return currentRoute && template === currentRoute.route.options.name ? 'active' : '';
     }
 });
-
-Template.nav.events({
-    "click #login-dropdown-list" : function(event){
-        var collapsed = $('#navbar').hasClass("navbar-collapse collapse");
-        var dropdownIsOpen = event.currentTarget.className === 'dropdown';
-        if(collapsed) {
-            if(dropdownIsOpen){
-                $("#loginSpacer").removeClass('visible-xs').addClass('visible-xs');
-            } else {
-                $("#loginSpacer").removeClass('visible-xs');
-            }
-        } else {
-            $("#loginSpacer").removeClass('visible-xs').addClass('visible-xs');
-        }
-    }
-});
